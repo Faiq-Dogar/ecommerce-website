de07f2a8bfce58c6d77a195ab23479b19af89eb0
myselfabdullah <myselfabdullah007@gmail.com>
2024-07-10 16:31:54 +0500

    About 10/July/24

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,6 +2,7 @@ import { Container } from '@mui/material'
 import React from 'react'
 import Griid from '../componenets/Griid'
 import TeamGrid from '../componenets/TeamGrid'
+import Features from '../componenets/Features'
 
 const About = ({aboutImage}) => {
   return (
@@ -9,6 +10,7 @@ const About = ({aboutImage}) => {
     <Container maxWidth="xl">
         <Griid aboutImage={aboutImage}/>
         <TeamGrid aboutImage={aboutImage}/>
+        <Features />
     </Container>
   )
 }
